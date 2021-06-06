3222667c0c02adc8284146be9c37316b42290e56
nicolas.continanza <nicolas.continanza@almundo.com>
2021-06-06 16:24:12 -0300

    Agrego modelos y termino el utils

---
+++ Parte2/utils.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def get_data():
+    GSPREADHSEET_DOWNLOAD_URL = (
+        "https://docs.google.com/spreadsheets/d/{gid}/export?format=csv&id={gid}".format
+    )
+    TP_GID = '1-DWTP8uwVS-dZY402-dm0F9ICw_6PNqDGLmH0u8Eqa0'
+    df = pd.read_csv(GSPREADHSEET_DOWNLOAD_URL(gid=TP_GID))
+    return df    
+
+def feature_engineering(df):
+    # Missings en barrio
+    data_set_mejorado = df.copy()
+    data_set_mejorado['barrio'] = data_set_mejorado['barrio'].apply(lambda x: 'Palermo' if str(x) == 'nan' else x)
+
+    # Missings en categoría de trabajo
+    data_set_mejorado['categoria_de_trabajo'] = data_set_mejorado['categoria_de_trabajo'].apply(lambda x: 'Sin categoria' if str(x) == 'nan' else x)
+
+    # Missings en trabajo
+    data_set_mejorado['trabajo'] = data_set_mejorado['trabajo'].apply(lambda x: 'No responde' if str(x) == 'nan' else x)
+
+    # Eliminación de el atributo eduación alcanzada
+    del data_set_mejorado['educacion_alcanzada']
+
+    # Juntamos casado y casada en una misma categoría.
+    data_set_mejorado['rol_familiar_registrado'] = data_set_mejorado['rol_familiar_registrado'].apply(lambda x : 'casado' if x == 'casada' else x)
+    
+    return data_set_mejorado
