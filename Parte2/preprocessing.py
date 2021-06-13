73405d893e52dee5d68c37ea82667d737476c939
nicolas.continanza <nicolas.continanza@almundo.com>
2021-06-13 17:29:37 -0300

    Armamos funcion de preprocessing con scaling y PCA

--- Parte2/preprocessing.py
+++ Parte2/preprocessing.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+
+
 
 # Funcion de preprocessing basada en el analisis realizado en la parte 1.
 def preprocessing_base_parte_1(df):
@@ -6,11 +10,20 @@ def preprocessing_base_parte_1(df):
     df_preproc = pd.get_dummies(df_preproc, drop_first=True)
     return df_preproc
 
-# Eliminamos las variables que consideramos que no nos aportan variabilidad
+
+def _log_scale(x):
+    return np.sign(x) * (np.log(abs(x)) + 1) if (x < -1 or x > 1) else x
+
 def preprocessing_significantes(df):
-    df_preproc = df[['ganancia_perdida_declarada_bolsa_argentina', 'edad', 'rol_familiar_registrado', 'anios_estudiados', 'genero', 'tiene_alto_valor_adquisitivo']]
-    df_preproc = pd.get_dummies(df_preproc, drop_first=True)
-    return df_preproc
+    scaler = MinMaxScaler()
+    pca = PCA(30)
+    
+    df = pd.get_dummies(df, drop_first=True)
+    df['ganancia_perdida_declarada_bolsa_argentina'] = df['ganancia_perdida_declarada_bolsa_argentina'].apply(_log_scale)
+
+    df = scaler.fit_transform(df)
+    return pca.fit_transform(df)
+
 
 # Tenemos en cuenta la representatividad poblacional y la estimamos
 def preprocessing_rep_poblacional(df):
