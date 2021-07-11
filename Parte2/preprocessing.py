0329d9a399019b94e78e6ffb43e84267bdcd491e
Lukas DAR <ldeangelis@fi.uba.ar>
2021-07-11 16:30:40 -0300

    Algunos modelos organizados
    Bagging / RandomForest / SVM / Tree

--- Parte2/preprocessing.py
+++ Parte2/preprocessing.py
@@ -23,22 +23,20 @@ def preprocessing_base_parte_1(X_train, X_test):
 def _log_scale(x):
     return np.sign(x) * (np.log(abs(x)) + 1) if (x < -1 or x > 1) else x
 
-def preprocessing_significantes(X_train, X_test, variance):
-    X_train = pd.get_dummies(X_train, drop_first=True)
-    X_test = pd.get_dummies(X_test, drop_first=True)
-    
+# Se asume que a X_train y a X_test ya han sido dummificadas.
+def preprocessing_significantes(X_train, X_test, variance):    
     pca_test = PCA()
     scaler = StandardScaler()
     
-    X_train['ganancia_perdida_declarada_bolsa_argentina'] = X_train['ganancia_perdida_declarada_bolsa_argentina'].apply(_log_scale)
-    X_test['ganancia_perdida_declarada_bolsa_argentina'] = X_test['ganancia_perdida_declarada_bolsa_argentina'].apply(_log_scale)
-    
+    X_train.loc[:, 'ganancia_perdida_declarada_bolsa_argentina'] = pd.Series(X_train['ganancia_perdida_declarada_bolsa_argentina']).apply(_log_scale)
+    X_test.loc[:, 'ganancia_perdida_declarada_bolsa_argentina'] = pd.Series(X_test['ganancia_perdida_declarada_bolsa_argentina']).apply(_log_scale)
+
     X_train_preproc = scaler.fit_transform(X_train)
     X_test_preproc = scaler.transform(X_test)
-    
+
     pca_test.fit(X_train_preproc)
     
-    n = (np.cumsum(pca.explained_variance_ratio_) > variance).index(True)
+    n = list(np.cumsum(pca_test.explained_variance_ratio_) > variance).index(True)
     pca = PCA(n)
     
     X_train_preproc = pd.DataFrame(pca.fit_transform(X_train_preproc))
@@ -49,10 +47,8 @@ def preprocessing_significantes(X_train, X_test, variance):
 
 # -
 
+# Se asume que a X_train y a X_test ya han sido dummificadas.
 def preprocessing_equilibrado(X_train, X_test, y_train, y_test):
-    X_train = pd.get_dummies(X_train, drop_first=True)
-    X_test = pd.get_dummies(X_test, drop_first=True)
-
     X_1 = X_train[y_train==1].copy()
     X_0 = X_train[y_train==0].copy()
 
@@ -61,7 +57,7 @@ def preprocessing_equilibrado(X_train, X_test, y_train, y_test):
     y_train_preproc = pd.concat([y_train[y_train==1],y_train.loc[indexs]])
 
     
-    return X_train_preproc, y_train_preproc, X_test, y_test
+    return X_train_preproc, X_test, y_train_preproc, y_test
 
 
 def preprocessing_4_mejores_variables_arbol(X_train, X_test):
